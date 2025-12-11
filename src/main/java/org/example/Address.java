3cbc3eed0ae4bf54e2c9426a196ee94791ade1b9
Callen <ctfinegan@gmail.com>
2025-12-11 15:38:42 -0500

    added getters and setters as well as toString, equals, and contructor. This finishes the Address class

--- src/main/java/org/example/Address.java
+++ src/main/java/org/example/Address.java
@@ -1,11 +1,15 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Address {
-    int streetNo;
-    String street;
-    String city;
-    Province province; // enum, contains abbriviations of all provinces in Canada
-    String postalCode;
+    private int streetNo;
+    private String street;
+    private String city;
+    private Province province;
+    private String postalCode;
+
+
 
     private static boolean isPostalCodeValid(String postalCode) {
         if (postalCode == null) {
@@ -31,8 +35,83 @@ public class Address {
     }
 
 
+    public Address(String postalCode, Province province, String city, String street, int streetNo) {
+        if (isPostalCodeValid(postalCode)) {
+            this.streetNo = streetNo;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.postalCode = postalCode.toUpperCase();
+        } else {
+            this.streetNo = 0;
+            this.street = null;
+            this.city = null;
+            this.province = null;
+            this.postalCode = null;
+        }
+    }
 
     public enum Province {
         AB, BC, MB, NB, NL, NS, ON, PE, QC, SK
     }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "streetNo= " + streetNo +
+                ", street= '" + street + '\'' +
+                ", city= '" + city + '\'' +
+                ", province= " + province +
+                ", postalCode= '" + postalCode + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return streetNo == address.streetNo && Objects.equals(street, address.street) && Objects.equals(city, address.city) && province == address.province && Objects.equals(postalCode, address.postalCode);
+    }
+
+    public int getStreetNo() {
+        return streetNo;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public Province getProvince() {
+        return province;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setStreetNo(int streetNo) {
+        this.streetNo = streetNo;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setProvince(Province province) {
+        this.province = province;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+
 }
