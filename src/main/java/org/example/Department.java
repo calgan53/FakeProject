a53a9d5c20dac1c553520906738eb6a25fea448f
Callen <ctfinegan@gmail.com>
2025-12-12 12:47:53 -0500

    Full Department Class

--- src/main/java/org/example/Department.java
+++ src/main/java/org/example/Department.java
@@ -1,4 +1,67 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Department {
+    String departmentId;
+    String departmentName;
+    static int nextId = 0;
+
+    static boolean isDepartmentNameValid(String departmentName) {
+        if (departmentName == null || departmentName.isEmpty()) {
+            return false;
+        }
+
+        for (int i = 0; i < departmentName.length(); i++) {
+            char c = departmentName.charAt(i);
+
+            // Check if the character is a letter OR a space
+            if (!Character.isLetter(c) && c != ' ') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public Department(String departmentName) {
+        if (isDepartmentNameValid(departmentName)) {
+            this.departmentName = departmentName;
+            this.departmentId = String.valueOf(nextId++);
+        }else{
+            this.departmentName = null;
+            this.departmentId = null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "departmentId='" + departmentId + '\'' +
+                ", departmentName='" + departmentName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return Objects.equals(departmentId, that.departmentId) && Objects.equals(departmentName, that.departmentName);
+    }
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public static int getNextId() {
+        return nextId;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
 }
